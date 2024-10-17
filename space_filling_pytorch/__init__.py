59dc8dcc3ba12d4131c096a801e704ebe01b72fd
Kitsunetic <jh.shim.gg@gmail.com>
2024-10-17 19:01:15 +0900

    make unified encode

--- space_filling_pytorch/__init__.py
+++ space_filling_pytorch/__init__.py
@@ -1,4 +1,3 @@
-from .functional.hilbert import (
-    point_to_hilbert_distance_3d_depth16_fp32 as hilbert_encode,
-)
+from .functional.hilbert import point_to_hilbert_distance_3d_depth16_fp32 as hilbert_encode
 from .functional.z_order import point_to_zorder_3d_depth16_fp32 as z_order_encode
+from .space_filling import encode
