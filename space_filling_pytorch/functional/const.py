a28a53d510e4148cc37065280c69f89c20fabf39
Kitsunetic <jh.shim.gg@gmail.com>
2024-10-29 11:39:46 +0900

    add const

--- space_filling_pytorch/functional/const.py
+++ space_filling_pytorch/functional/const.py
@@ -1,2 +1,8 @@
+import random
+
 METHODS = ["hilbert", "z"]
 CONVENTIONS = ["xyz", "xzy", "yxz", "yzx", "zxy", "zyx"]
+
+
+def sample_random_method():
+    return random.choice(METHODS), random.choice(CONVENTIONS)
